a68e732bf5c01cc0be4d5918029d22acb8e4d062
merlinfrombelgium <merlinfrombelgium@gmail.com>
2024-11-20 11:56:10 +0100

    Updates

---
+++ static/js/952.a6eaa7f7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkoffice_presence=self.webpackChunkoffice_presence||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.a6eaa7f7.chunk.js.map
